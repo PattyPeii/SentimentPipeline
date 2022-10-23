c5aa88985a22b30b6239eb6142e1a89d1e6f7889
Natchanon Suviriyapaisal <few.natchanon@gmail.com>
2022-10-23 17:23:00 +0000

    Initial commit

---
+++ core/save_db.py
@@ -0,0 +1,31 @@
+import pymongo
+from bson.objectid import ObjectId
+
+class DB_CONNECT(object):
+    def __init__(self, config: dict):
+        self.config = config
+        #self.timezone = datetime.datetime.strptime(self.config["timezone"], "%z").tzinfo
+        self.db = self.db_init(self.config)
+
+    def db_init(self, config):
+        # Provide the mongodb atlas url to connect python to mongodb using pymongo
+        CONNECTION_STRING = config["database"]["connection"]
+        # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+        client = pymongo.MongoClient(CONNECTION_STRING)
+        return client
+
+    def save_msg(self, msg):
+        db = self.db
+        db_connect = db[self.config["database"]["name"]]
+        collection = db_connect[self.config["database"]["collections"]["comments"]]
+        res = collection.insert_one(msg)
+        return res.inserted_id
+
+    def update_msg(self, id, msg):
+        filter = { '_id': ObjectId(id) }
+        db = self.db
+        db_connect = db[self.config["database"]["name"]]
+        collection = db_connect[self.config["database"]["collections"]["comments"]]
+        newvalues = { "$set": { "comment": msg["comment"] , "sentiment":msg["sentiment"]} }
+        collection.update_one(filter, newvalues)
+        return id
