80081d678ea11e430f214465fafb3dbbe5da1f8e
snatchanon <few.natchanon@gmail.com>
2022-11-09 01:33:48 +0700

    Make it round-robin

--- consume.py
+++ consume.py
@@ -9,21 +9,10 @@ connection = pika.BlockingConnection(
     pika.URLParameters(config["host"]))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
-
-result = channel.queue_declare(queue='', exclusive=True)
+result = channel.queue_declare(queue="comment", exclusive=False)
 queue_name = result.method.queue
 
-severities = sys.argv[1:]
-if not severities:
-    sys.stderr.write("Usage: %s [comment]\n" % sys.argv[0])
-    sys.exit(1)
-for severity in severities:
-    channel.queue_bind(
-        exchange='direct_logs', queue=queue_name, routing_key=severity)
-
 print(' [*] Waiting for logs. To exit press CTRL+C')
-
 
 def callback(ch, method, properties, body):
     print(" [x] %r:%r" % (method.routing_key, body))
